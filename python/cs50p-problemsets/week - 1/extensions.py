710865c9152f10f70f2589841c79d6937486d264
ekansh gupta <gekansh83@gmail.com>
2026-05-26 22:15:37 +0530

    python: cs50p week1 file-extensions problem set complete

---
+++ python/cs50p-problemsets/week - 1/extensions.py
@@ -0,0 +1,24 @@
+filename = input("File name: ").strip().lower()
+
+if filename.endswith(".gif"):
+    print("image/gif")
+
+elif filename.endswith(".jpeg") or filename.endswith(".jpg"):
+    print("image/jpeg")
+
+elif filename.endswith(".png"):
+     print("image/png")
+
+elif filename.endswith(".txt"):
+    print("text/plain")
+
+elif filename.endswith(".pdf"):
+    print("application/pdf")
+
+elif filename.endswith(".zip"):
+    print("application/zip")
+
+else:
+    print("application/octet-stream")
+
+
