d48a12697556106809b6812c14e1cd58748f59ee
ekansh gupta <gekansh83@gmail.com>
2026-07-06 23:27:21 +0530

    python: cs50p week-5 test_fuel complete

---
+++ python/cs50p-problemsets/week-5/test_fuel.py
@@ -0,0 +1,35 @@
+import pytest
+from fuel import convert, gauge
+
+def test_convert_valid():
+    assert convert("3/4") == 75
+    assert convert("1/4") == 25
+    assert convert("1/100") == 1
+    assert convert("0/4") == 0
+    assert convert("99/100") == 99
+    assert convert("1/1") == 100
+
+def test_convert_zero_division():
+    with pytest.raises(ZeroDivisionError):
+        convert("1/0")
+
+def test_convert_value_error():
+    with pytest.raises(ValueError):
+        convert("5/4")
+    with pytest.raises(ValueError):
+        convert("three/four")
+    with pytest.raises(ValueError):
+        convert("3.5/4")
+    with pytest.raises(ValueError):
+        convert("-1/4")
+    with pytest.raises(ValueError):
+        convert("1/-4")
+    with pytest.raises(ValueError):
+        convert("-3/-4")        
+
+def test_gauge():
+    assert gauge(0) == "E"
+    assert gauge(1) == "E"
+    assert gauge(50) == "50%"
+    assert gauge(99) == "F"
+    assert gauge(100) == "F"
